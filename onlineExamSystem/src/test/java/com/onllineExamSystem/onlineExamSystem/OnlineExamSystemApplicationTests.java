e8a0d2c139d78c8a637ca0d6eb48f52b85c6f98b
Parth Patil <parth253pp@outlook.com>
2024-12-11 23:44:20 +0530

    Online Exam System Project Configuration

---
+++ onlineExamSystem/src/test/java/com/onllineExamSystem/onlineExamSystem/OnlineExamSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onllineExamSystem.onlineExamSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineExamSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
