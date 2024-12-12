90568cdea2b4948fe7dc0b03a4819facf84b5914
MrsDhanashri <165988325+MrsDhanashri@users.noreply.github.com>
2024-12-12 14:21:37 +0530

    created one controller method demo created demo.html inside template/user used thymleaf inside demo.html

--- onlineExamSystem/src/main/java/com/onlineExamSystem/controller/OnlineExamSystemController.java
+++ onlineExamSystem/src/main/java/com/onlineExamSystem/controller/OnlineExamSystemController.java
@@ -1,5 +1,16 @@
 package com.onlineExamSystem.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class OnlineExamSystemController {
 
+	@GetMapping("/demo")
+	public String demo(Model model) {
+		model.addAttribute("name","ram");
+		return "user/demo";
+		
+	}
 }
