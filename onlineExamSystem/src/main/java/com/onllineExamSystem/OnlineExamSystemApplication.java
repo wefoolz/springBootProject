c48e69e481f3ad60052288d3400363ef80122d8c
Parth Patil <parth253pp@outlook.com>
2024-12-12 07:27:18 +0530

    Updating the package name com.onlineExamSystem.onlineExamSyste to com.onlineExamSystem

--- onlineExamSystem/src/main/java/com/onllineExamSystem/OnlineExamSystemApplication.java
+++ onlineExamSystem/src/main/java/com/onllineExamSystem/OnlineExamSystemApplication.java
@@ -1,4 +1,4 @@
-package com.onllineExamSystem.onlineExamSystem;
+package com.onllineExamSystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
