1c1f9440e09852059d4156eb6b54caeafd2d9df5
swaroop2477 <swaroopshinge1999@gmail.com>
2024-12-30 18:43:36 +0530

    30-12-24 1. linking & adding bootsstrap file ,js file applying css properties on sidebar 2. creating button and added css&js

---
+++ onlineExamSystem/src/main/resources/static/js/StudentDashboard.js
@@ -0,0 +1,14 @@
+/* logout button  start */
+var logoutBtn =document.querySelector("#logout-btn");
+logoutBtn.onclick =function(){
+    this.innerHTML=" Logging out . . .";
+    logoutBtn.disabled = true;
+    this.style.background ="#ccc";
+  //   changes for parth     
+    setTimeout(function(){
+        window.location = "/onlineExamSystem/src/main/resources/templates/base.html" 
+        sessionStorage.removeItem =("/")
+    },500);
+}
+
+/* logout button  end */
